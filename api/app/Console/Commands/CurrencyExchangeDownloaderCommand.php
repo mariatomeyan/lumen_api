04b3059c25a98488b13244bd4d0b15fa22dc2dba
mariyatomeyan <mariya.tomeyan@omo-health.com>
2022-08-04 12:45:33 +0200

    -created command that will dowload the currencies with php artisan

--- api/app/Console/Commands/CurrencyExchangeDownloaderCommand.php
+++ api/app/Console/Commands/CurrencyExchangeDownloaderCommand.php
@@ -1,12 +1,15 @@
 <?php
 namespace App\Console\Commands;
+use App\Events\DownloadCurrencyRatesEvent;
+use App\Services\Currency;
+use App\Services\ExchangeRateService;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Http;
 
 class CurrencyExchangeDownloaderCommand extends Command {
 
     protected $signature = 'download:currency:rates';
-    protected $description = 'Downloads currency rates!';
+    protected $description = 'Downloads currency rates';
+
 
     public function __construct()
     {
@@ -15,7 +18,7 @@ class CurrencyExchangeDownloaderCommand extends Command {
 
     public function handle()
     {
-        $response = Http::get('https://v6.exchangerate-api.com/v6/b9355d54a2ca68fa63838170/latest/EUR');
-        $this->info($response->body());
+        $service =  new ExchangeRateService(Currency::EUR);
+        event(new DownloadCurrencyRatesEvent($service));
     }
 }
